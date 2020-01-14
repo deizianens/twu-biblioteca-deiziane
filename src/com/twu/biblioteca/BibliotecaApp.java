355854d29a78ff68de975467e90e916b7d1b042b
Deiziane Silva <deiziane_ns@hotmail.com>
2020-01-14 14:02:21 -0300

    Add menu exit

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -9,7 +9,7 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n");
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
 
         Book pp = new Book("The Little Prince", "Antoine de Saint-Exupéry", 1943);
         Book ventos = new Book("Pride and Prejudice", "Jane Austen", 1813);
@@ -20,12 +20,19 @@ public class BibliotecaApp {
         bookList.add(ventos);
         bookList.add(gt);
 
-        System.out.println("Choose one option:\n1.See list of books");
-        Integer option = input.nextInt();
+        while(true) {
+            System.out.println("\nChoose one option:\n1.See list of books\n0.Quit\n");
+            Integer option = input.nextInt();
 
-        switch (option) {
-            case 1:
-                getBooksList(bookList);
+            switch (option) {
+                case 1:
+                    getBooksList(bookList);
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("Please select a valid option!\n");
+            }
         }
     }
 
